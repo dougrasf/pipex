83bb1e9ccce7eb707515ba0a45ddebc69ad8babb
dofranci <dofranci@student.42sp.org.br>
2022-11-10 23:05:02 -0300

    lib

---
+++ libft/src/ft_strtrim.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dofranci <dofranci@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 19:17:32 by dofranci          #+#    #+#             */
+/*   Updated: 2022/06/24 17:53:01 by dofranci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	size;
+	
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	size = ft_strlen(s1);
+	while (size && ft_strchr(set, s1[size]))
+		size--;
+	return (ft_substr((char *)s1, 0, size + 1));
+}
