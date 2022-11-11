0b8d5daab779146feb85a5e7fcaf88bcfd68edc4
dofranci <dofranci@student.42sp.org.br>
2022-11-10 22:55:28 -0300

    pipe with 2 comands ok, need error treatment and corrections in especifics commands like awk

---
+++ pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dofranci <dofranci@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 20:24:14 by dofranci          #+#    #+#             */
+/*   Updated: 2022/11/10 22:38:03 by dofranci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <unistd.h>
+#include "./libft/include/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+
+#endif
